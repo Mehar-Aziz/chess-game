3d6d50f7d118335c4374549d125a3c184df7b160
Mehar-Aziz <meharaziz.1614>
2024-07-14 14:46:29 +0500

    Chess game

---
+++ main.py
@@ -0,0 +1,11 @@
+row1 = ["😀", "😀", "😀"]
+row2 = ["😀", "😀", "😀"]
+row3 = ["😀", "😀", "😀"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put teasure? ")
+row = int(position[0])-1
+column = int (position[1])-1
+map[column][row] = "X"
+print(f"{row1}\n{row2}\n{row3}")
